2e69e9336dba5fe7b09928efb0886ddf0ca34a91
zhongyiquan <zhongyiquan@autobio.com.cn>
2021-03-16 12:13:34 +0800

    添加demosql

---
+++ demo.sql
@@ -0,0 +1,121 @@
+/*
+SQLyog Ultimate - MySQL GUI v8.2 
+MySQL - 5.5.27 : Database - sql_optimization
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`sql_optimization` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+
+USE `sql_optimization`;
+
+/*Table structure for table `addr` */
+
+DROP TABLE IF EXISTS `addr`;
+
+CREATE TABLE `addr` (
+  `ID` int(11) NOT NULL,
+  `ADDR` varchar(128) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `addr` */
+
+/*Table structure for table `friend` */
+
+DROP TABLE IF EXISTS `friend`;
+
+CREATE TABLE `friend` (
+  `USER_ID` int(11) DEFAULT NULL,
+  `FRIEND_ID` int(11) DEFAULT NULL,
+  KEY `idx_user_id` (`USER_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `friend` */
+
+/*Table structure for table `menu` */
+
+DROP TABLE IF EXISTS `menu`;
+
+CREATE TABLE `menu` (
+  `ID` int(11) NOT NULL,
+  `MENU_NAME` varchar(128) COLLATE utf8_bin DEFAULT NULL,
+  `MENU_URL` varchar(128) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `menu` */
+
+insert  into `menu`(`ID`,`MENU_NAME`,`MENU_URL`) values (1,'用户新增','api/User/Add'),(2,'用户删除','api/User/Delete'),(3,'用户修改','api/User/Update'),(4,'用户查询','api/User/Query'),(5,'角色新增','api/Role/Add'),(6,'角色修改','api/Role/Update'),(7,'角色删除','api/Role/Delete'),(8,'角色查询','api/Role/Query');
+
+/*Table structure for table `role` */
+
+DROP TABLE IF EXISTS `role`;
+
+CREATE TABLE `role` (
+  `ID` int(11) NOT NULL,
+  `ROLE_NAME` varchar(128) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `role` */
+
+insert  into `role`(`ID`,`ROLE_NAME`) values (1,'admin'),(2,'test'),(3,'custmor'),(4,'role1'),(5,'role2');
+
+/*Table structure for table `role_menu` */
+
+DROP TABLE IF EXISTS `role_menu`;
+
+CREATE TABLE `role_menu` (
+  `ID` int(11) NOT NULL,
+  `ROLE_ID` int(11) DEFAULT NULL,
+  `MENU_ID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `role_menu` */
+
+insert  into `role_menu`(`ID`,`ROLE_ID`,`MENU_ID`) values (1,1,1),(2,1,2),(3,1,3),(4,1,4),(5,1,5),(6,1,6),(7,1,7),(8,1,8),(9,2,1),(10,2,2),(11,2,3),(12,3,5),(13,3,6),(14,3,7),(15,4,1),(16,4,5),(17,4,8),(18,5,3);
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `ID` int(11) NOT NULL,
+  `USER_NAME` varchar(128) COLLATE utf8_bin DEFAULT NULL,
+  `USER_PWD` varchar(128) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `idx_user_name` (`USER_NAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `user` */
+
+insert  into `user`(`ID`,`USER_NAME`,`USER_PWD`) values (1,'Zoe','123456'),(2,'Coder','23452a'),(3,'Code综艺圈','231235');
+
+/*Table structure for table `user_role` */
+
+DROP TABLE IF EXISTS `user_role`;
+
+CREATE TABLE `user_role` (
+  `ID` int(11) NOT NULL,
+  `USER_ID` int(11) DEFAULT NULL,
+  `ROLE_ID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `user_role` */
+
+insert  into `user_role`(`ID`,`USER_ID`,`ROLE_ID`) values (1,1,1),(2,2,2),(3,2,3),(4,3,2),(5,3,5);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
